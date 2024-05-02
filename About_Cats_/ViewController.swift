d5d3266961d3f33eef0c50b8a21e1fd411a7b30e
vako03 <valeri.mekhashishvili97@gmail.com>
2024-05-02 10:31:32 +0400

    Initial Commit

---
+++ About_Cats_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About_Cats_
+//
+//  Created by vako on 02.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
