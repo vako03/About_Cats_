80ba5191843cd2f1014ef38ac80911849af25bb2
vako03 <valeri.mekhashishvili97@gmail.com>
2024-05-03 03:02:57 +0400

    changed some code

--- About_Cats_/Cell/CustomCell.swift
+++ About_Cats_/Cell/CustomCell.swift
@@ -8,7 +8,11 @@
 import UIKit
 import MyLibraryAboutCats
 
+
 class CustomCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
     let factLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
@@ -17,27 +21,31 @@ class CustomCell: UITableViewCell {
         label.textColor = .black
         return label
     }()
-
+    
+    // MARK: - Initializers
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(factLabel)
-
+        
         NSLayoutConstraint.activate([
             factLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             factLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             factLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             factLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
         ])
-
+        
         backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
         layer.cornerRadius = 10
         clipsToBounds = true
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    // MARK: - Cell Configuration
+    
     func configure(with text: String) {
         factLabel.text = text
     }
--- About_Cats_/ViewController/ViewController.swift
+++ About_Cats_/ViewController/ViewController.swift
@@ -8,42 +8,49 @@ import UIKit
 import MyLibraryAboutCats
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
     var tableView: UITableView!
     var viewModel = CatFactsViewModel()
-
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .gray
         setupUI()
         fetchCatFacts()
     }
-
+    
+    // MARK: - UI Setup
+    
     private func setupUI() {
-       
-        let navigationController = UINavigationController(rootViewController: self)
-        navigationController.navigationBar.prefersLargeTitles = true
-        self.navigationController?.navigationBar.tintColor = .black
         
-       
-        self.navigationItem.title = "List of Facts"
-        
-       
-        tableView = UITableView(frame: navigationController.view.bounds, style: .plain)
+        tableView = UITableView(frame: .zero, style: .plain)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
         tableView.separatorStyle = .none
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
+        
         view.addSubview(tableView)
         
-       
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-
+    
+    
+    
+    // MARK: - Data Fetching
+    
     private func fetchCatFacts() {
         viewModel.fetchFacts { [weak self] in
             DispatchQueue.main.async {
@@ -53,19 +60,21 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Table View Data Source and Delegate
+
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.facts.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
         let fact = viewModel.facts[indexPath.row]
         cell.factLabel.text = fact.fact
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100 
+        return 150
     }
 }
