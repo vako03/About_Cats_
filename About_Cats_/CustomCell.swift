83d1f15eb3d06386c1e5a25a5b4d1a582f844650
vako03 <valeri.mekhashishvili97@gmail.com>
2024-05-02 12:16:52 +0400

    added some constraints

---
+++ About_Cats_/CustomCell.swift
@@ -0,0 +1,44 @@
+//
+//  CustomCell.swift
+//  About_Cats_
+//
+//  Created by vako on 02.05.24.
+//
+
+import UIKit
+import MyLibraryAboutCats
+
+class CustomCell: UITableViewCell {
+    let factLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(factLabel)
+
+        NSLayoutConstraint.activate([
+            factLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            factLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            factLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            factLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+
+        backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with text: String) {
+        factLabel.text = text
+    }
+}
--- About_Cats_/ViewController.swift
+++ About_Cats_/ViewController.swift
@@ -4,16 +4,68 @@
 //
 //  Created by vako on 02.05.24.
 //
-
 import UIKit
+import MyLibraryAboutCats
 
 class ViewController: UIViewController {
+    var tableView: UITableView!
+    var viewModel = CatFactsViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        fetchCatFacts()
     }
 
+    private func setupUI() {
+       
+        let navigationController = UINavigationController(rootViewController: self)
+        navigationController.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.tintColor = .black
+        
+       
+        self.navigationItem.title = "List of Facts"
+        
+       
+        tableView = UITableView(frame: navigationController.view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+        tableView.separatorStyle = .none
+        view.addSubview(tableView)
+        
+       
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
+    private func fetchCatFacts() {
+        viewModel.fetchFacts { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.facts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        let fact = viewModel.facts[indexPath.row]
+        cell.factLabel.text = fact.fact
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100 
+    }
+}
